bab0991a610eadfe19018ee15ff4d6e58dd215cc
PowerPenguini <jaskiewiczteo@gmail.com>
2023-07-27 15:45:58 +0200

    Add vim-plug to installation file

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Run as root
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 apt install ripgrep
